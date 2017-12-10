30ae2e5e44180bfc71984bc53e2f75847ae11f74
James Gary <bucket@earthlink.net>
2017-12-10 15:13:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function theBeatlesPlay(musicians, instruments)
+{
+  var new_arr = [];
+  for (var counter = 0; counter <= musicians.length-1; counter ++)
+  {
+    new_arr.push(musicians[counter]+" plays " +instruments[counter]);
+  }
+  return new_arr;
+}
